ce4c7ac2b73e86c386049c6e8ef3a1f01eddb286
Gabi Bronshtein <gabi.bronshtein@gmail.com>
2024-06-16 12:24:44 +0300

    Implement solution

--- server.py
+++ server.py
@@ -1,7 +1,24 @@
+import random
 import requests
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+
+def get_meal_options(main_ingredient: str) -> list:
+    return requests.get(f"https://www.themealdb.com/api/json/v1/1/filter.php?i={main_ingredient}").json()["meals"]
+
+
+def get_full_recipe(meal_id: int) -> dict:
+    return requests.get(f"https://www.themealdb.com/api/json/v1/1/lookup.php?i={meal_id}").json()["meals"][0]
+
+
+def format_ingredients(full_recipe: dict) -> dict:
+    return {
+        full_recipe[f"strIngredient{index}"]: full_recipe[f"strMeasure{index}"]
+        for index in range(1, 21)
+        if full_recipe[f"strIngredient{index}"]
+    }
 
 
 def get_recipe_for(main_ingredient: str) -> dict:
@@ -9,36 +26,27 @@ def get_recipe_for(main_ingredient: str) -> dict:
     Fetch a random recipe with a specified main ingredient.
     The recipes are requested from "themealdb" API.
     """
-    # YOUR CODE HERE:
-    # 1. Use `requests` to fetch a list of recipes with `main_ingredient`
-    #    - Here is a useful URL: www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast
-    #    - Hint: `response.json()`
-    # 2. Choose a random recipe from the list
-    #    - Hint: `random.choice()`
-    # 3. Use `requests` again to fetch the full recipe instructions
-    #    - Here is another useful URL: www.themealdb.com/api/json/v1/1/lookup.php?i=52772
+    selected_meal = random.choice(get_meal_options(main_ingredient))
+    full_recipe = get_full_recipe(selected_meal["idMeal"])
     # 4. Fill in the details of the returned object in the format below
     return dict(
-        name="",
-        image="",
-        video="",
-        instructions="",
-        ingredients=dict(
-            # ingredient: measure,
-        )
+        name=full_recipe["strMeal"],
+        image=full_recipe["strMealThumb"],
+        video=full_recipe["strYoutube"],
+        instructions=full_recipe["strInstructions"],
+        ingredients=format_ingredients(full_recipe)
     )
 
 
 @app.route('/')
-def index():
+def index_page():
     """
     Renders and returns the main page HTML.
     """
     return render_template("index.html")
 
 
-# YOUR CODE HERE:
-# Make this endpoint get called when for the following routes: "/recipe/<ingredient>"
+@app.route('/recipe/<ingredient>')
 def recipe(ingredient):
     """
     Renders and returns an HTML page with a random recipe for a given main ingredient.
@@ -48,10 +56,9 @@ def recipe(ingredient):
                            recipe=get_recipe_for(ingredient))
 
 
-# YOU CODE HERE:
-# Add another endpoint:
-# Route "/recipe/<ingredient>/json" should return a JSON with the output of `get_recipe_for`.
-# Hint: `flask.jsonify()`
+@app.route('/recipe/<ingredient>/json')
+def recipe_json(ingredient):
+    return jsonify(get_recipe_for(ingredient))
 
 
 if __name__ == '__main__':
